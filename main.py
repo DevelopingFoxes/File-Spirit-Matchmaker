d160e18a94c5f651672b82cdbe909597d9476977
Paw <you@example.com>
2024-07-03 16:08:17 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 15:47:04 2024
+
+@author: _
+"""
+
+## TODO
+
+## CLI INTERFACE
+
+## Input: <Foler path to Scan Dir>
+### DOCS: Scan folder (non recursive) and make a multi-dimentional list [file_no_extetion, file_with_extention, file path]
+
+## Input: <Folder Path to Scan Dir>
+### DOCS: Scan folder (non recursive) and make a --//--
+
+
+### Input: <Folder Path to Output results>
+'''
+Save path for later.
+Later: Move all files with matching names regardless of file extention. 
+If mulitple files have the same full-file-name, add a count to the newer file, --> file.file, file-1.file
+'''
+
+
+## Input: <confirm OR add folder path>
+'''
+Display current config, confirm to proceed or to add another folder input.
+
+'''
+
+text = input("prompt")
+
+
