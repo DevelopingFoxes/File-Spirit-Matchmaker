f190850535b2f34dcd5c5795b2a97d72faabe126
DevelopingFoxes <dev+github@dhelprave.com>
2024-08-23 21:45:43 +0200

    WIP: Add search dir arguments to argparse

--- FSM.py
+++ FSM.py
@@ -1,4 +1,3 @@
-
 # === IMPORTS ===
 
 # CLI Argument handling
@@ -10,14 +9,62 @@ import pathlib
 
 # === CLI Argument Parsing ===
 
-def cli_argparse():
-    """ 
+
+def command_line_interface_argument_parser():
+    """
     Parsers the passed arguments and flags from a terminal interface when calling this program.
     """
+
+    # Todo: Checklist
+    # Add custom 'usage' parameters for a more understadible help menu
+
+    # Todo: Arguments
+    # Postional:
+    # Source,
+    # Search...
+    # Non positional:
+    # Source:<path...>,
+    # Search:<path...>,
+    # Destination:<path...>
+    # Grouped Action Flags (only allow one):
+    # Copy,
+    # Move,
+    # --to-file:<path|| -d:path >
+    # Misc arguments:
+    # quiet,
+    # verbose(include debug/log levels in code),
+    # force (dunno for what),
+    # dry-run (similar to --to-file but prints to terminal)
+    # "dont-include-source-findings-except-for-matched-pairs" arguments (but a better name)
+
+    # Initiates argparser
+    # passed argumnet allows line/breaks in help text argument
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+
+    # -- Source Group --
+    source_args = parser.add_argument_group(
+        "Source",
+        description="Source directory arguments. \nOne or multiple directories as a basis to compare files from \n",
+    )
+    source_args.add_argument(
+        "source",
+        action="store",
+        nargs="?",
+        # type=pathlib.Path,
+        help="Single positional argument for source directory path",
+    )
+    source_args.add_argument(
+        "-s",
+        "--source",
+        # action="append",
+        nargs='+',
+        # type=pathlib.Path,
+        help="Specifies single or mulitple source direcetory paths",
+    )
+    args = parser.parse_args()
     
-    # Todo: We want the following
-    # Postional: Source, Search
-    # Non positional: Source, Search, Destination
-    # Grouped Action Flags (only allow one): Copy, Move, --to-file:<path|| -d:path >
-    # Misc arguments: quiet, verbose(include debug/log levels in code), force, dry-run (similar to --to-file but prints to terminal)
-    # Add custom 'usage' parameters for a more understandible help menu
+    # Dev: TEMP OUTPUT
+    print(args)
+
+# === RUNNING ===
+command_line_interface_argument_parser()
